4bfb1d50662a29c3eaf0380726e611f52db77d2f
Renzo Lucioni <renzo@lucioni.xyz>
2017-04-10 11:03:40 -0400

    Add parametrization todo

--- courtbot/tests/test_utils.py
+++ courtbot/tests/test_utils.py
@@ -9,5 +9,6 @@ class TestConversationalJoin:
     def test_two_items(self):
         pass
 
+    # TODO: https://docs.pytest.org/en/latest/parametrize.html
     def test_three_or_more_items(self):
         pass
